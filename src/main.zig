d787e33760b994201c3b192e223fd843ada7c1b7
Tachera Sasi <tacherasasi@gmail.com>
2025-11-26 10:10:13 +0300

    Update window title to "Zig Invaders" and enhance drawing logic

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,18 @@ pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 600;
 
-    rl.initWindow(screenWidth, screenHeight, "Zig Raylib Example");
+    rl.initWindow(screenWidth, screenHeight, "Zig Invaders");
 
     defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.black);
+
+        rl.drawText("Zig Invaders!", 300, 250, 40, rl.Color.green);
+    }
 }
